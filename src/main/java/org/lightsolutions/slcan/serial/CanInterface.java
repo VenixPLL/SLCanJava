158598af96d50d5cf57ab083392ce8d607c61130
VenixPLL <saddist@wp.pl>
2026-03-26 21:00:07 +0100

    Remove unused imports in CanInterface.java

--- src/main/java/org/lightsolutions/slcan/serial/CanInterface.java
+++ src/main/java/org/lightsolutions/slcan/serial/CanInterface.java
@@ -12,9 +12,7 @@ import org.lightsolutions.slcan.canbus.CanVersion;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.*;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 /**
  * Threaded SLCAN serial interface.
